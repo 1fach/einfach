b1284e9f77fe70a6978912a6aa5a99ac402a029c
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2024-07-14 18:23:23 +0200

    🏷️ add vite as devDependency to fix type error

--- apps/web/app/db/sqlite.ts
+++ apps/web/app/db/sqlite.ts
@@ -2,7 +2,6 @@ import { Factory, SQLITE_OPEN_READONLY, SQLITE_ROW } from 'wa-sqlite'
 
 import SQLiteAsyncESMFactory from 'wa-sqlite/dist/wa-sqlite-async.mjs'
 
-/* @ts-expect-error wasm url type */
 import wasmUrl from 'wa-sqlite/dist/wa-sqlite-async.wasm?url'
 import { IDBBatchAtomicVFS } from './idb/IDBBatchAtomicVFS'
 
